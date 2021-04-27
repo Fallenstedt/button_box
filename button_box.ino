3f19036c7e36e1f62288399035993aec399fbe79
Alex Fallenstedt <a.fallenstedt@gmail.com>
2021-04-26 18:53:03 -0700

    big button state

--- button_box.ino
+++ button_box.ino
@@ -1,77 +1,88 @@
-// https://github.com/MHeironimus/ArduinoJoystickLibrary
+#include <Keypad.h>
 #include <Joystick.h>
 
-//https://playground.arduino.cc/Code/Keypad/
-#include <Keypad.h>
+//DEFINITIONS
+#define NUMBUTTONS 16 //replace "?"with number of buttong you are using
+#define NUMROWS 4 //replace "?" with number of rows you have
+#define NUMCOLS 4 //replace "?" with number of columns you have
 
-// Big Switch
-const byte BIG_SWITCH_PIN = 13;
+// BIG BUTTN
+#define BIG_SWITCH_PIN 12
 byte BIG_SWITCH_STATE = LOW;
 
-
-// Keypad
-const byte rows = 4;
-const byte cols = 4;
-char keys[rows][cols] = {
-  {'a', 'b', 'c', 'd'},
-  {'e', 'f', 'g', 'h'},
-  {'i', 'j', 'k', 'l'},
-  {'m', 'n', 'o', 'p'},
+//BUTTON MATRIX
+//first change number of rows and columns to match your button matrix,
+//then replace all "?" with numbers (starting from 0)
+byte buttons[NUMROWS][NUMCOLS] = {
+  {1, 2, 3, 4},
+  {5, 6, 7, 8},
+  {9, 10, 11, 12},
+  {13, 14, 15, 16}
 };
-byte rowPins[rows] = {2, 3, 4, 5};
-byte colPins[cols] = {6, 7, 8, 9};
-Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
 
-//initialize an Joystick with 16 buttons;
+//BUTTON MATRIX PART 2
+byte rowPins[NUMROWS] = {2, 3, 4, 5}; //change "?" to the pins the rows of your button matrix are connected to
+byte colPins[NUMCOLS] = {6, 7, 8, 9}; //change "?" to the pins the rows of your button matrix are connected to
+
+Keypad buttbx = Keypad( makeKeymap(buttons), rowPins, colPins, NUMROWS, NUMCOLS);
+
+//JOYSTICK SETTINGS
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
-                   JOYSTICK_TYPE_JOYSTICK, 16, 0,
-                   false, false, false, false, false, false,
-                   false, false, false, false, false);
+                   JOYSTICK_TYPE_GAMEPAD,
+                   18, //number of buttons
+                   0, //number of hat switches
+                   //Set as many axis to "true" as you have potentiometers for
+                   false, // y axis
+                   false, // x axis
+                   false, // z axis
+                   false, // rx axis
+                   false, // ry axis
+                   false, // rz axis
+                   false, // rudder
+                   false, // throttle
+                   false, // accelerator
+                   false, // brake
+                   false); // steering wheel
 
 
 void setup() {
-  Serial.begin(9600); // open the serial port at 9600 bps:
-
+  Serial.begin(9600);
   Joystick.begin();
   pinMode(BIG_SWITCH_PIN, INPUT);
 }
 
 void loop() {
-
   CheckAllButtons();
-
 }
 
 
 void CheckAllButtons(void) {
-  if (!keypad.getKeys()) {
-    return;
-  }
-
-  // Button Grid
-  for (int i = 0; i < LIST_MAX; i++) {
-    if (keypad.key[i].stateChanged) {
-      switch (keypad.key[i].kstate) {
-        case PRESSED:
-        case HOLD:
-          Joystick.setButton(keypad.key[i].kchar, 1);
-          Serial.println(keypad.key[i].kchar);
-          break;
-        case RELEASED:
-        case IDLE:
-          Joystick.setButton(keypad.key[i].kchar, 0);
-          break;
-          
-      }
-    }
-  }
-
-      // Big Switch
   byte s = digitalRead(BIG_SWITCH_PIN);
   if (s != BIG_SWITCH_STATE) {
     BIG_SWITCH_STATE = s;
+    Joystick.setButton(17, BIG_SWITCH_STATE);
     delay(1);
-    Serial.println(BIG_SWITCH_STATE);
+    
   }
-
+  
+  if (buttbx.getKeys())
+  {
+    for (int i = 0; i < LIST_MAX; i++)
+    {
+      if ( buttbx.key[i].stateChanged )
+      {
+        switch (buttbx.key[i].kstate) {
+          case PRESSED:
+          case HOLD:
+            Serial.println("press");
+            Joystick.setButton(buttbx.key[i].kchar, 1);
+            break;
+          case RELEASED:
+          case IDLE:
+            Joystick.setButton(buttbx.key[i].kchar, 0);
+            break;
+        }
+      }
+    }
+  }
 }
